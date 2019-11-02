43dbc4c56dd1556ef749b1a2025efb30541d7531
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-02 12:53:50 +0000

    separate reader Module

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import json
+import subprocess
+from reader import Reader
 
 with open('config.json', 'r') as f:
     settings = json.load(f)
 
 print(settings)
+
+def call(*args):
+    subprocess.call([settings['chromecastCliPath'], '-H', settings['chromecastIP']] + list(args))
+
+call('status')
+
+with Reader(1, 1) as reader:
+    reader.read()
