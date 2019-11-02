ef0660df0c5596e8b517703494e0ed1f4e53a551
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-02 13:36:39 +0000

    evdev

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import subprocess
-from reader import Reader
+from keyboard import Reader
 
 with open('config.json', 'r') as f:
     settings = json.load(f)
@@ -12,5 +12,29 @@ def call(*args):
 
 call('status')
 
-with Reader(1, 1) as reader:
-    reader.read()
+while True:
+    try:
+        with Reader('/dev/input/event0') as reader:
+            line = reader.read()
+            found = False
+            for mapping in settings['cardMappings']:
+                if mapping['code'] == line:
+                    found = True
+                    print("Playing %s" % mapping['name'])
+                    call('play', mapping['url'])
+
+            if not found:
+                print("No mapping found for code %s" % line)
+                for i in range(len(settings['cardMappings'])):
+                    print("%i: %s" % (i, settings['cardMappings'][i]))
+                new = int(raw_input("Select new mapping"))
+                settings['cardMappings'][new]['code'] = line
+                with open('config.json', 'w') as f:
+                    json.dump(settings, f)
+
+                print(settings['cardMappings'][new])
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print(e)
+        continue
